d66408ef26497749d39147539035f9be7d3b49d3
paulomaateus <paulo.moreira@ccc.ufcg.edu.br>
2020-08-06 15:19:03 -0300

    Front end finalizado

---
+++ scripts/addField.js
@@ -0,0 +1,12 @@
+//Funcionalidade de adicionar um novo campo para horarios de dar aulas 
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+function cloneField(){
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    const fields = newFieldContainer.querySelectorAll('input')
+    fields.forEach(function(field){
+        field.value = ""
+    })
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
